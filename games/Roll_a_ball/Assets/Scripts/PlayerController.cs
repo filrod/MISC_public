8919033a3d7c73151fc9c7d34478b470dea5e67c
Filipe Rodrigues <39252535+filrod@users.noreply.github.com>
2018-09-27 15:14:46 -0400

    my Roll a ball
    Stock version

---
+++ games/Roll_a_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    // Create a rigid body insance
+    private Rigidbody rb;
+
+    // Create a speed field
+    public float speed;
+
+    // Create a counter for the coins
+    private int count;
+
+    // Create text variable for count
+    public Text countText;
+
+    // Create text variable for win
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Set count to zero
+        count = 0;
+
+        // Set Count text
+        set_countText();
+
+        winText.text = "";
+    }
+
+    // Check every frame for player input
+    // Update called before rendering frame
+    void Update()
+    {
+        
+    }
+
+    // Physics code applies
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Add a 3Vector for force
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  // Initialized
+
+        rb.AddForce(movement*speed);
+    }
+
+    // Create a setter for countText
+    void set_countText() {
+
+        // Set Count text
+        countText.text = "Coins: " + count.ToString();
+
+        // 
+        if (count>=12){
+            winText.text = "You Win!";
+        }
+    }
+
+    // A trigger method to check if objects will be picked up
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+           
+            other.gameObject.SetActive(false);
+            // Increment the count
+            count += 1;
+
+            // Change the displayed count
+            set_countText();
+        }
+    }
+}
---
+++ games/Roll_a_ball/Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Rotate the coins to grab player's attention
+        transform.Rotate(new Vector3(0, 0, 60) * Time.deltaTime);  // Mult makes smoother
+		
+	}
+}
