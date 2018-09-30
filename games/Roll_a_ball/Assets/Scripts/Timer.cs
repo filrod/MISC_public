c6142c647a9ec314e9d498da6464855363766b02
Filipe Rodrigues <39252535+filrod@users.noreply.github.com>
2018-09-30 10:25:20 -0400

    Solving problems w/ versions

---
+++ games/Roll_a_ball/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float time;
+    public Text timeText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time = time - Time.deltaTime;
+
+        timeText.text = "Time: " + time.ToString("00");
+
+        if (time < -0.2)
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
