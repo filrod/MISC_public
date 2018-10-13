5280134a088fc8af53fe6da768c3b06661beed85
Filipe Rodrigues <39252535+filrod@users.noreply.github.com>
2018-10-12 23:29:37 -0400

    changed controls

--- games/Like_Clockwork/Like Clockwork/Assets/Scripts/WheelTansforms.cs
+++ games/Like_Clockwork/Like Clockwork/Assets/Scripts/WheelTansforms.cs
@@ -14,24 +14,78 @@ public class WheelTansforms : MonoBehaviour {
     public float maxTorque = 50;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
 
         car = GetComponent<Rigidbody>();
 
         // Set low center of mass
         car.centerOfMass = new Vector3(0, -0.8f, 0);
-	}
-	
-	// Called several times per frame (physics)
-	void FixedUpdate () {
-
-        wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
-        wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
-
-        // Stearing 
-        wheelFR.steerAngle = 45 * Input.GetAxis("Horizontal");
-        wheelFL.steerAngle = 45 * Input.GetAxis("Horizontal");
-
-
     }
+
+    // Called several times per frame (physics)
+    void FixedUpdate()
+    {
+      
+        // Reverse 
+        float reverse = 0f;
+        if (Input.GetKey(KeyCode.Joystick1Button1)==true)
+        {
+            reverse -= 0.05f;
+            wheelRR.motorTorque = (float)Math.Pow(maxTorque, 2) * reverse;
+            wheelRL.motorTorque = (float)Math.Pow(maxTorque, 2) * reverse;
+        }
+        else {
+            reverse = 0f;
+            float drive = Input.GetAxis("Right Trigger");
+            float brake = Input.GetAxis("Left Trigger");
+
+
+            Acceleration(wheelRR, drive);
+            Acceleration(wheelRL, drive);
+
+            wheelRR.brakeTorque = brake * float.MaxValue/1.1f;
+            wheelRL.brakeTorque = brake * float.MaxValue /1.1f;
+
+            if (wheelRR.rpm > 500)
+                wheelRR.motorTorque = 0;
+
+            if (wheelRL.rpm > 500)
+                wheelRL.motorTorque = 0;
+        }
+
+        //}
+        // Stearing 
+        float steer = Input.GetAxis("Horizontal");
+        wheelFR.steerAngle = 30 * steer;
+        wheelFL.steerAngle = 30 * steer;
+    }
+
+    void Acceleration(WheelCollider wheel, float drive) {
+        /*
+         * Acceleration factor dependant on rpm.
+         */
+
+        if (wheel.rpm < 6 && drive > 0.1)
+        {
+            wheelRR.motorTorque = float.MaxValue;
+        }
+        else if (wheel.rpm < 30 && drive > 0.1)
+        {
+            wheelRR.motorTorque = 0.8f * float.MaxValue;
+        }
+        else if (wheel.rpm < 80 && drive > 0.1)
+        {
+            wheelRR.motorTorque = 0.25f * float.MaxValue;
+        }
+        else if (wheel.rpm < 90 && drive > 0.1)
+        {
+            wheelRR.motorTorque = 0.05f * float.MaxValue;
+        }
+        else
+        {
+            wheel.motorTorque = (float)Math.Pow(maxTorque, 3)
+                                * drive;
+        }
+    }
+    
 }
