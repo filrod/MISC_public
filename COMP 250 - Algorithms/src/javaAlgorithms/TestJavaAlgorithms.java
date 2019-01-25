244d9140732a1ca63554c212a57cbd380a076872
Filipe Rodrigues <39252535+filrod@users.noreply.github.com>
2019-01-25 12:19:48 -0500

    Attempt of importing JUnit and testing
    Still doesn't work

---
+++ COMP 250 - Algorithms/src/javaAlgorithms/TestJavaAlgorithms.java
@@ -0,0 +1,15 @@
+package javaAlgorithms;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import org.junit.Test;
+
+public class TestJavaAlgorithms {
+
+	/*@Test
+	public static void binSearchTest() {
+		
+		assertEquals();
+	}
+	*/
+}
