5c7da8209f1c8c9c3aaabf8022197540b6895861
Filipe Rodrigues <39252535+filrod@users.noreply.github.com>
2019-03-08 13:35:45 -0500

    Menu loads map
    Revamped the main menu
    The start button now loads the desert map
    Exported this to a build
    Todo:
    More maps

---
+++ games/Like_Clockwork/Like Clockwork/Assets/Scripts/Main_Menu/Load_desertMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_desertMap : MonoBehaviour
+{
+    public void loadDestertMap()
+    {
+        SceneManager.LoadScene(1);  // Loads desert map track specified to be scene 1 in the build
+    }
+}
